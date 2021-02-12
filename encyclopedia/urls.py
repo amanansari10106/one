280affbea480271a33ebb3a1f224350e69530dd2
Aman Ansari <amanansari10106@gmail.com>
2021-02-12 10:34:38 +0530

    hii

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.conf.urls import url
 from . import views
-from django.conf import settings
+
 urlpatterns = [
     path("", views.index, name="index"),
     # path("a/", views.viewz, name="viewz")
